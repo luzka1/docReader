ab86b2040044a5674e896d638fd558c1b5acd00a
Lucas <lucasgsantos1727@gmail.com>
2025-08-05 18:54:07 -0300

    refactor: testando filtragem de dados que não vão ser utilizados

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,12 +1,14 @@
 import Papa from "papaparse";
 import { useEffect, useState } from "react";
 
+type TableData = { fornecedor: string; valor: string };
+
 function Table() {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<TableData[]>([]);
 
   useEffect(() => {
     const url =
-      "https://docs.google.com/spreadsheets/d/1m9SgVTpHCHvdWotPIhywuuynxtL7VfqM/gviz/tq?tqx=out:csv&gid=1368580946";
+      "https://docs.google.com/spreadsheets/d/1T5OKSCPCBdz39vvSS4ZwykuFn50IuqyB/gviz/tq?tqx=out:csv&gid=1368580946";
 
     fetch(url)
       .then((response) => response.text())
@@ -25,13 +27,15 @@ function Table() {
           complete: (results: any) => {
             const rawData = results.data;
 
+            console.log(rawData);
+
             const keys = Object.keys(rawData[0]);
             const fornecedorKey = keys.find((key) =>
               key.toLowerCase().includes("fornecedor")
             );
             const valorKey = keys.find(
               (key) =>
-                key.toLowerCase().includes("pago") ||
+                key.toLowerCase().includes("original") ||
                 key.toLowerCase().includes("valor")
             );
 
@@ -56,6 +60,8 @@ function Table() {
       });
   }, []);
 
+  const filter = ["REGISTROS", "Correios", "GERAL"];
+
   return (
     <div className="p-12 flex flex-col gap-12 overflow-y-auto">
       <div>
@@ -69,40 +75,29 @@ function Table() {
       </div>
 
       <div className="flex flex-col items-center xl:grid xl:grid-cols-3 gap-8">
-        {Array.from({ length: 4 }).map((_, index) => (
-          <div
-            key={index}
-            className="w-sm h-60 rounded-4xl border shadow-md p-6"
-          >
-            <div className="h-full w-full flex justify-between flex-col">
-              <h2 className="font-bold text-lg">Nome do Dentista</h2>
+        {data
+          .filter((value) => value.fornecedor.includes("TOTAL") && !value.fornecedor.includes(filter[0]))
+          .map((item, index) => (
+            <div
+              key={index}
+              className="w-sm h-60 rounded-4xl border shadow-md p-6"
+            >
+              <div className="h-full w-full flex justify-between flex-col">
+                <h2 className="font-bold text-lg">{item.fornecedor}</h2>
 
-              <div className="flex flex-row gap-2 justify-between">
-                <div className="flex flex-col w-1/2">
-                  <p className="text-sm text-gray-500">Comissão paga</p>
-                  <p className="text-lg font-semibold">R$ 1.000,00</p>
-                </div>
-                <div className="flex flex-col w-1/2">
-                  <p className="text-sm text-gray-500">Comissão a pagar</p>
-                  <p className="text-lg font-semibold">R$ 1.000,00</p>
-
-                  {data.map((item, index) => (
-                    <div
-                      key={index}
-                      className="w-sm h-40 rounded-4xl border shadow-md p-6"
-                    >
-                      <h2 className="font-bold text-lg">{item.fornecedor}</h2>
-                      <p className="text-sm text-gray-500 mt-2">
-                        Valor a pagar:
-                      </p>
-                      <p className="text-lg font-semibold">{item.valor}</p>
-                    </div>
-                  ))}
+                <div className="flex flex-row gap-2 justify-between">
+                  <div className="flex flex-col w-1/2">
+                    <p className="text-sm text-gray-500">Valor original</p>
+                    <p className="text-lg font-semibold">{item.valor}</p>
+                  </div>
+                  {/* <div className="flex flex-col w-1/2">
+                    <p className="text-sm text-gray-500">Comissão a pagar</p>
+                    <p className="text-lg font-semibold">R$ 1.000,00</p>
+                  </div> */}
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
